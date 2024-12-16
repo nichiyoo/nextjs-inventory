89542a3f16b96c810ce31ef28367af2da844f1d6
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2024-12-16 19:28:44 +0700

    feat: add production env setting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,18 @@ import 'dotenv/config';
 
 import { defineConfig } from 'drizzle-kit';
 
+const production = {
+	url: process.env.TURSO_DATABASE_URL!,
+	authToken: process.env.TURSO_AUTH_TOKEN!,
+};
+
+const development = {
+	url: process.env.DATABASE_FILE_NAME!,
+};
+
 export default defineConfig({
 	out: './drizzle',
 	schema: './database/schema.ts',
-	dialect: 'sqlite',
-	dbCredentials: {
-		url: process.env.DATABASE_FILE_NAME!,
-	},
+	dialect: process.env.NODE_ENV === 'production' ? 'turso' : 'sqlite',
+	dbCredentials: process.env.NODE_ENV === 'production' ? production : development,
 });
